f0fa4b13beaee17b9b1fdd4197edf06bd0b9de89
Vitaliy <600merin.174@mail.ru>
2019-04-18 18:52:05 +0300

    Это первый коммит

---
+++ lesson1-hard/src.js
@@ -0,0 +1,6 @@
+var num = '33721',
+    a = num[0] * num[1] * num[2] * num[3] * num[4];
+console.log(a);
+var x = a ** 3;
+console.log(String(x).substr(0, 2)); // две первые цифры
+// console.log(String(x).substr(-2)); две последние цифры
---
+++ lesson1/js.js
@@ -0,0 +1,17 @@
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: {},
+    savings: false
+};
+let a = prompt("Введите обязательную статью расходов в этом месяце",""),
+    b = prompt("Во сколько обойдется",""),
+    c = prompt("Введите обязательную статью расходов в этом месяце",""),
+    d = prompt("Во сколько обойдется","");
+appData.expenses.a / b;
+appData.expenses.c / d;
+alert("Ваш бюджет на день:" + appData.budget / 30);
